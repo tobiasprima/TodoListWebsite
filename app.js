bf777fe8618839e46ac8e8ffb3f6cdc343e897e1
Tobias <primatjahja@gmail.com>
2023-10-11 14:11:20 +0800

    Refactor the Code

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
-
+    res.render("app")
 })
 
 app.listen(port, function(){
